e1be070b5a70a06a28dfc4c895e1121ca86efa4a
Tiago Dias <tiago@Tiagos-MacBook-Pro.local>
2019-04-07 15:33:06 +0100

    refurbished stowage tool

--- README.md
+++ README.md
@@ -1,5 +1,23 @@
-These are my personal configurations. This is obviously very tailored to my personal use,
+# dotfiles
+
+These are my personal configurations.
+This is obviously very tailored to my personal use,
 although it might contain things others would find useful.
 
-System is based on GNU stow, which symlinks everything into the home directory. 
-Simply run `tool.sh` for more info.
+Each sub-directory in this repo is a "package", which is installed into the
+system using the `tool.sh` script.  
+
+## Package configuration
+
+Packages can have a `META` file inside which will change install configurations.
+
+* **MODE**
+
+  `STOW` (default): Directory will be installed as a GNU stow package (each file
+    will be symlinked)  
+  `SYMLINK`: Directory will be symlinked
+
+* **DEST**
+
+  Base directory for install. Defaults to `$HOME`.  
+  Files inside each package will be mapped into this directory.
--- tool.sh
+++ tool.sh
@@ -1,21 +1,72 @@
 #!/usr/bin/env bash
 
-cmd=(stow -d . -t "$HOME" --no-folding --verbose)
+# go to script dir
+cd "$(dirname "$0")"
 
-case "$1" in
-	up)
-	${cmd[@]} */
-	;;
-	
-	down)
-	${cmd[@]} --delete */
-	;;
+usage() {
+	echo "Usage: $0 <up|down>"
+}
 
-	reload)
-	${cmd[@]} --restow */
-	;;
+for pack in */; do
+	# remove trailing /
+	pack=${pack%?};
 
-	*)
-	echo "Usage: $0 <up|down|reload>"
-	;;
-esac
+	# defaults
+	MODE=STOW
+	DEST=$HOME
+
+	# load META
+	source "$pack/_META" 2>/dev/null
+
+	echo "----------------------"
+	echo "Package: $pack"
+	echo "Mode:    $MODE"
+	echo "Dest:    $DEST"
+
+	if [ "$1" = "up" ]; then
+		if [ "$MODE" = "STOW" ]; then
+			# stow
+			stow \
+				--stow \
+				-d "$PWD" \
+				-t "$DEST" \
+				--no-folding \
+				--ignore '_META' \
+				"$pack"
+
+		elif [ "$MODE" = "SYMLINK" ]; then
+			# symlink
+			if [ -e "$DEST/$pack" ]; then
+				if [ ! -h "$DEST/$pack" ]; then
+					echo "Destination already exists, and is not a symlink."
+				else
+					echo "Symlink already exists"
+				fi
+			else
+				ln -s "$PWD/$pack" "$DEST"
+			fi
+		fi
+
+	elif [ "$1" = "down" ]; then
+		if [ "$MODE" == "STOW" ]; then
+			# unstow
+			stow \
+				--delete \
+				-d "$PWD" \
+				-t "$DEST" \
+				"$pack"
+
+		elif [ "$MODE" = "SYMLINK" ]; then
+			# unsymlink
+			if [ -h "$DEST/$pack" ]; then
+				rm "$DEST/$pack"
+
+			elif [ -e "$DEST/$pack" ]; then
+				echo "$DEST/$pack is not a symlink. Can't delete."
+
+			else
+				echo "$DEST/$pack does not exist."
+			fi
+		fi
+	fi
+done
